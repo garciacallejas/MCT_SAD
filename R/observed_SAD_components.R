d02e6135d9c465c7e81693691494212fd00f292c
garciacallejas <david.garcia.callejas@gmail.com>
2020-12-01 20:11:15 +0100

    .

---
+++ R/observed_SAD_components.R
@@ -0,0 +1,65 @@
+
+library(tidyverse)
+source("R/hill_diversity.R")
+
+# read data ---------------------------------------------------------------
+
+abund.obs <- read.csv2("../Caracoles/data/abundances.csv",header = TRUE,
+                       stringsAsFactors = FALSE)
+
+# this just to constrain abundances to the same set of sp as the other files
+alpha.df <- read.csv2("results/alpha.csv",stringsAsFactors = FALSE,
+                      row.names = 1)
+alpha.matrix <- as.matrix(alpha.df)
+
+sp.rates <- read.csv2("../Caracoles/data/plant_species_traits.csv",
+                      header = TRUE,stringsAsFactors = FALSE)
+sp.valid <- sp.rates$species.code[which(!is.na(sp.rates$germination.rate))]
+
+# extract components ------------------------------------------------------
+
+plot.obs <- abund.obs %>%
+  filter(species %in% sp.valid & species %in% rownames(alpha.matrix)) %>%
+  filter(!is.na(individuals) &
+           individuals > 0) %>%
+  group_by(year,plot,species) %>%
+  summarise(mean.abund = mean(individuals),sum.abund = sum(individuals))
+
+plot.obs <- plot.obs[,c("year","plot","species",
+                        "mean.abund","sum.abund")]
+
+years <- sort(unique(plot.obs$year))
+plots <- sort(unique(plot.obs$plot))
+components <- c("abundance","richness","evenness")
+
+rad.components <- expand.grid(year = years,
+                              plot = plots,
+                              richness = NA_real_,
+                              abundance = NA_real_,
+                              evenness = NA_real_)
+
+for(i.year in 1:length(years)){
+  for(i.plot in 1:length(plots)){
+      my.rad <- subset(plot.obs,year == years[i.year] & 
+                         plot == plots[i.plot])
+      
+      pos <- which(rad.components$year == years[i.year] & 
+                     rad.components$plot == plots[i.plot])
+      
+      rad.components$abundance[pos] <- sum(my.rad$sum.abund)
+      rad.components$richness[pos] <- nrow(my.rad)
+      rad.components$evenness[pos] <- hill.diversity(my.rad$sum.abund)
+  }
+}
+
+# store results -----------------------------------------------------------
+
+rad.components.long <- gather(rad.components, 
+                              key = "metric", 
+                              value = "value", 
+                              abundance,richness,evenness)
+
+rad.components.long$type <- "observed"
+
+write.csv2(rad.components.long,file = "results/observed_SAD_components.csv",
+           row.names = FALSE)
--- R/predict_SAD_components.R
+++ R/predict_SAD_components.R
@@ -57,83 +57,7 @@ for(i.year in 1:length(initial.years)){
         sp.alpha <- communities[[initial.years[i.year]]][[plots[i.plot]]][[types[i.type]]][["alpha"]]
         abund.df <- communities[[initial.years[i.year]]][[plots[i.plot]]][[types[i.type]]][["abundances"]]
         
-        sp.lambda <- lambda.df$lambda
-        names(sp.lambda) <- lambda.df$sp
-        
-        sp.abund <- abund.df$abundance
-        names(sp.abund) <- abund.df$species
-        
-        sub.abund <- cxr::abundance_projection(lambda = sp.lambda,
-                                               alpha_matrix = sp.alpha,
-                                               model_family = model_family,
-                                               alpha_form = alpha_form,
-                                               lambda_cov_form = lambda_cov_form,
-                                               alpha_cov_form = alpha_cov_form,
-                                               timesteps = 2, # because t1 is the original
-                                               initial_abundances = sp.abund)
-        projected.abund <- sub.abund[2,]
-        projected.composition <- sum(projected.abund > 0)
-        projected.abundance <- sum(projected.abund)
-        projected.evenness <- hill.diversity(projected.abund)
-        
-        pos <- which(pert.sad$year.predicted == (as.numeric(initial.years[i.year]) + 1) &
-                       pert.sad$plot == plots[i.plot] & 
-                       pert.sad$type == "obs" )
-        
-        pert.sad$value[pos[which(pert.sad$metric[pos] == "composition")]] <- 
-          projected.composition
-        pert.sad$value[pos[which(pert.sad$metric[pos] == "abundance")]] <- 
-          projected.abundance
-        pert.sad$value[pos[which(pert.sad$metric[pos] == "evenness")]] <- 
-          projected.evenness
-        
-      }else if(types[i.type] == "nd"){
-        
-        for(i.step in 1:steps){
-        
-        lambda.df <- communities[[initial.years[i.year]]][[plots[i.plot]]][[types[i.type]]][["lambda"]]
-        sp.alpha <- communities[[initial.years[i.year]]][[plots[i.plot]]][[types[i.type]]][["alpha"]][[i.step]]
-        abund.df <- communities[[initial.years[i.year]]][[plots[i.plot]]][[types[i.type]]][["abundances"]]
-        
-        sp.lambda <- lambda.df$lambda
-        names(sp.lambda) <- lambda.df$sp
-        
-        sp.abund <- abund.df$abundance
-        names(sp.abund) <- abund.df$species
-        
-        sub.abund <- cxr::abundance_projection(lambda = sp.lambda,
-                                               alpha_matrix = sp.alpha,
-                                               model_family = model_family,
-                                               alpha_form = alpha_form,
-                                               lambda_cov_form = lambda_cov_form,
-                                               alpha_cov_form = alpha_cov_form,
-                                               timesteps = 2, # because t1 is the original
-                                               initial_abundances = sp.abund)
-        projected.abund <- sub.abund[2,]
-        projected.composition <- sum(projected.abund > 0)
-        projected.abundance <- sum(projected.abund)
-        projected.evenness <- hill.diversity(projected.abund)
-        
-        pos <- which(pert.sad$year.predicted == (as.numeric(initial.years[i.year]) + 1) &
-                       pert.sad$plot == plots[i.plot] & 
-                       pert.sad$type == types[i.type] &
-                       pert.sad$intensity == i.step)
-        
-        pert.sad$value[pos[which(pert.sad$metric[pos] == "composition")]] <- 
-          projected.composition
-        pert.sad$value[pos[which(pert.sad$metric[pos] == "abundance")]] <- 
-          projected.abundance
-        pert.sad$value[pos[which(pert.sad$metric[pos] == "evenness")]] <- 
-          projected.evenness
-        
-        }# for i.step
-      }else if(types[i.type] == "fd"){
-        
-        for(i.step in 1:steps){
-          
-          lambda.df <- communities[[initial.years[i.year]]][[plots[i.plot]]][[types[i.type]]][["lambda"]][[i.step]]
-          sp.alpha <- communities[[initial.years[i.year]]][[plots[i.plot]]][[types[i.type]]][["alpha"]]
-          abund.df <- communities[[initial.years[i.year]]][[plots[i.plot]]][[types[i.type]]][["abundances"]]
+        if(sum(sum(is.na(lambda.df)),sum(is.na(sp.alpha)),sum(is.na(abund.df))) == 0){
           
           sp.lambda <- lambda.df$lambda
           names(sp.lambda) <- lambda.df$sp
@@ -156,8 +80,7 @@ for(i.year in 1:length(initial.years)){
           
           pos <- which(pert.sad$year.predicted == (as.numeric(initial.years[i.year]) + 1) &
                          pert.sad$plot == plots[i.plot] & 
-                         pert.sad$type == types[i.type] &
-                         pert.sad$intensity == i.step)
+                         pert.sad$type == "obs" )
           
           pert.sad$value[pos[which(pert.sad$metric[pos] == "composition")]] <- 
             projected.composition
@@ -165,7 +88,98 @@ for(i.year in 1:length(initial.years)){
             projected.abundance
           pert.sad$value[pos[which(pert.sad$metric[pos] == "evenness")]] <- 
             projected.evenness
+        }# if !na
+      }else if(types[i.type] == "nd"){
+        
+        for(i.step in 1:steps){
           
+          lambda.df <- communities[[initial.years[i.year]]][[plots[i.plot]]][[types[i.type]]][["lambda"]]
+          if(sum(is.na(lambda.df)) == 0){
+            sp.alpha <- communities[[initial.years[i.year]]][[plots[i.plot]]][[types[i.type]]][["alpha"]][[i.step]]
+          }else{
+            sp.alpha <- NA
+          }
+          abund.df <- communities[[initial.years[i.year]]][[plots[i.plot]]][[types[i.type]]][["abundances"]]
+          
+          if(sum(sum(is.na(lambda.df)),sum(is.na(sp.alpha)),sum(is.na(abund.df))) == 0){
+            
+            sp.lambda <- lambda.df$lambda
+            names(sp.lambda) <- lambda.df$sp
+            
+            sp.abund <- abund.df$abundance
+            names(sp.abund) <- abund.df$species
+            
+            sub.abund <- cxr::abundance_projection(lambda = sp.lambda,
+                                                   alpha_matrix = sp.alpha,
+                                                   model_family = model_family,
+                                                   alpha_form = alpha_form,
+                                                   lambda_cov_form = lambda_cov_form,
+                                                   alpha_cov_form = alpha_cov_form,
+                                                   timesteps = 2, # because t1 is the original
+                                                   initial_abundances = sp.abund)
+            projected.abund <- sub.abund[2,]
+            projected.composition <- sum(projected.abund > 0)
+            projected.abundance <- sum(projected.abund)
+            projected.evenness <- hill.diversity(projected.abund)
+            
+            pos <- which(pert.sad$year.predicted == (as.numeric(initial.years[i.year]) + 1) &
+                           pert.sad$plot == plots[i.plot] & 
+                           pert.sad$type == types[i.type] &
+                           pert.sad$intensity == i.step)
+            
+            pert.sad$value[pos[which(pert.sad$metric[pos] == "composition")]] <- 
+              projected.composition
+            pert.sad$value[pos[which(pert.sad$metric[pos] == "abundance")]] <- 
+              projected.abundance
+            pert.sad$value[pos[which(pert.sad$metric[pos] == "evenness")]] <- 
+              projected.evenness
+          }# if !na
+        }# for i.step
+      }else if(types[i.type] == "fd"){
+        
+        for(i.step in 1:steps){
+          
+          sp.alpha <- communities[[initial.years[i.year]]][[plots[i.plot]]][[types[i.type]]][["alpha"]]
+          abund.df <- communities[[initial.years[i.year]]][[plots[i.plot]]][[types[i.type]]][["abundances"]]
+          if(sum(is.na(sp.alpha)) == 0){
+            lambda.df <- communities[[initial.years[i.year]]][[plots[i.plot]]][[types[i.type]]][["lambda"]][[i.step]]
+          }else{
+            sp.alpha <- NA
+          }
+          
+          if(sum(sum(is.na(lambda.df)),sum(is.na(sp.alpha)),sum(is.na(abund.df))) == 0){
+            
+            sp.lambda <- lambda.df$lambda
+            names(sp.lambda) <- lambda.df$sp
+            
+            sp.abund <- abund.df$abundance
+            names(sp.abund) <- abund.df$species
+            
+            sub.abund <- cxr::abundance_projection(lambda = sp.lambda,
+                                                   alpha_matrix = sp.alpha,
+                                                   model_family = model_family,
+                                                   alpha_form = alpha_form,
+                                                   lambda_cov_form = lambda_cov_form,
+                                                   alpha_cov_form = alpha_cov_form,
+                                                   timesteps = 2, # because t1 is the original
+                                                   initial_abundances = sp.abund)
+            projected.abund <- sub.abund[2,]
+            projected.composition <- sum(projected.abund > 0)
+            projected.abundance <- sum(projected.abund)
+            projected.evenness <- hill.diversity(projected.abund)
+            
+            pos <- which(pert.sad$year.predicted == (as.numeric(initial.years[i.year]) + 1) &
+                           pert.sad$plot == plots[i.plot] & 
+                           pert.sad$type == types[i.type] &
+                           pert.sad$intensity == i.step)
+            
+            pert.sad$value[pos[which(pert.sad$metric[pos] == "composition")]] <- 
+              projected.composition
+            pert.sad$value[pos[which(pert.sad$metric[pos] == "abundance")]] <- 
+              projected.abundance
+            pert.sad$value[pos[which(pert.sad$metric[pos] == "evenness")]] <- 
+              projected.evenness
+          }# if !na
         }# for i.step
         
       }else if(types[i.type] == "ia"){
@@ -173,40 +187,47 @@ for(i.year in 1:length(initial.years)){
         for(i.step in 1:steps){
           
           lambda.df <- communities[[initial.years[i.year]]][[plots[i.plot]]][[types[i.type]]][["lambda"]]
-          sp.alpha <- communities[[initial.years[i.year]]][[plots[i.plot]]][[types[i.type]]][["alpha"]][[i.step]]
+          if(sum(is.na(lambda.df)) == 0){
+            sp.alpha <- communities[[initial.years[i.year]]][[plots[i.plot]]][[types[i.type]]][["alpha"]][[i.step]]
+          }else{
+            sp.alpha <- NA
+          }
           abund.df <- communities[[initial.years[i.year]]][[plots[i.plot]]][[types[i.type]]][["abundances"]]
           
-          sp.lambda <- lambda.df$lambda
-          names(sp.lambda) <- lambda.df$sp
-          
-          sp.abund <- abund.df$abundance
-          names(sp.abund) <- abund.df$species
-          
-          sub.abund <- cxr::abundance_projection(lambda = sp.lambda,
-                                                 alpha_matrix = sp.alpha,
-                                                 model_family = model_family,
-                                                 alpha_form = alpha_form,
-                                                 lambda_cov_form = lambda_cov_form,
-                                                 alpha_cov_form = alpha_cov_form,
-                                                 timesteps = 2, # because t1 is the original
-                                                 initial_abundances = sp.abund)
-          projected.abund <- sub.abund[2,]
-          projected.composition <- sum(projected.abund > 0)
-          projected.abundance <- sum(projected.abund)
-          projected.evenness <- hill.diversity(projected.abund)
-          
-          pos <- which(pert.sad$year.predicted == (as.numeric(initial.years[i.year]) + 1) &
-                         pert.sad$plot == plots[i.plot] & 
-                         pert.sad$type == types[i.type] &
-                         pert.sad$intensity == i.step)
-          
-          pert.sad$value[pos[which(pert.sad$metric[pos] == "composition")]] <- 
-            projected.composition
-          pert.sad$value[pos[which(pert.sad$metric[pos] == "abundance")]] <- 
-            projected.abundance
-          pert.sad$value[pos[which(pert.sad$metric[pos] == "evenness")]] <- 
-            projected.evenness
-          
+          if(sum(sum(is.na(lambda.df)),sum(is.na(sp.alpha)),sum(is.na(abund.df))) == 0){
+            
+            sp.lambda <- lambda.df$lambda
+            names(sp.lambda) <- lambda.df$sp
+            
+            sp.abund <- abund.df$abundance
+            names(sp.abund) <- abund.df$species
+            
+            sub.abund <- cxr::abundance_projection(lambda = sp.lambda,
+                                                   alpha_matrix = sp.alpha,
+                                                   model_family = model_family,
+                                                   alpha_form = alpha_form,
+                                                   lambda_cov_form = lambda_cov_form,
+                                                   alpha_cov_form = alpha_cov_form,
+                                                   timesteps = 2, # because t1 is the original
+                                                   initial_abundances = sp.abund)
+            projected.abund <- sub.abund[2,]
+            projected.composition <- sum(projected.abund > 0)
+            projected.abundance <- sum(projected.abund)
+            projected.evenness <- hill.diversity(projected.abund)
+            
+            pos <- which(pert.sad$year.predicted == (as.numeric(initial.years[i.year]) + 1) &
+                           pert.sad$plot == plots[i.plot] & 
+                           pert.sad$type == types[i.type] &
+                           pert.sad$intensity == i.step)
+            
+            pert.sad$value[pos[which(pert.sad$metric[pos] == "composition")]] <- 
+              projected.composition
+            pert.sad$value[pos[which(pert.sad$metric[pos] == "abundance")]] <- 
+              projected.abundance
+            pert.sad$value[pos[which(pert.sad$metric[pos] == "evenness")]] <- 
+              projected.evenness
+            
+          }# if !na
         }# for i.step
         
       }else if(types[i.type] == "id"){
@@ -214,40 +235,46 @@ for(i.year in 1:length(initial.years)){
         for(i.step in 1:steps){
           
           lambda.df <- communities[[initial.years[i.year]]][[plots[i.plot]]][[types[i.type]]][["lambda"]]
-          sp.alpha <- communities[[initial.years[i.year]]][[plots[i.plot]]][[types[i.type]]][["alpha"]][[i.step]]
+          if(sum(is.na(lambda.df)) == 0){
+            sp.alpha <- communities[[initial.years[i.year]]][[plots[i.plot]]][[types[i.type]]][["alpha"]][[i.step]]
+          }else{
+            sp.alpha <- NA
+          }
           abund.df <- communities[[initial.years[i.year]]][[plots[i.plot]]][[types[i.type]]][["abundances"]]
           
-          sp.lambda <- lambda.df$lambda
-          names(sp.lambda) <- lambda.df$sp
-          
-          sp.abund <- abund.df$abundance
-          names(sp.abund) <- abund.df$species
-          
-          sub.abund <- cxr::abundance_projection(lambda = sp.lambda,
-                                                 alpha_matrix = sp.alpha,
-                                                 model_family = model_family,
-                                                 alpha_form = alpha_form,
-                                                 lambda_cov_form = lambda_cov_form,
-                                                 alpha_cov_form = alpha_cov_form,
-                                                 timesteps = 2, # because t1 is the original
-                                                 initial_abundances = sp.abund)
-          projected.abund <- sub.abund[2,]
-          projected.composition <- sum(projected.abund > 0)
-          projected.abundance <- sum(projected.abund)
-          projected.evenness <- hill.diversity(projected.abund)
-          
-          pos <- which(pert.sad$year.predicted == (as.numeric(initial.years[i.year]) + 1) &
-                         pert.sad$plot == plots[i.plot] & 
-                         pert.sad$type == types[i.type] &
-                         pert.sad$intensity == i.step)
-          
-          pert.sad$value[pos[which(pert.sad$metric[pos] == "composition")]] <- 
-            projected.composition
-          pert.sad$value[pos[which(pert.sad$metric[pos] == "abundance")]] <- 
-            projected.abundance
-          pert.sad$value[pos[which(pert.sad$metric[pos] == "evenness")]] <- 
-            projected.evenness
-          
+          if(sum(sum(is.na(lambda.df)),sum(is.na(sp.alpha)),sum(is.na(abund.df))) == 0){
+            
+            sp.lambda <- lambda.df$lambda
+            names(sp.lambda) <- lambda.df$sp
+            
+            sp.abund <- abund.df$abundance
+            names(sp.abund) <- abund.df$species
+            
+            sub.abund <- cxr::abundance_projection(lambda = sp.lambda,
+                                                   alpha_matrix = sp.alpha,
+                                                   model_family = model_family,
+                                                   alpha_form = alpha_form,
+                                                   lambda_cov_form = lambda_cov_form,
+                                                   alpha_cov_form = alpha_cov_form,
+                                                   timesteps = 2, # because t1 is the original
+                                                   initial_abundances = sp.abund)
+            projected.abund <- sub.abund[2,]
+            projected.composition <- sum(projected.abund > 0)
+            projected.abundance <- sum(projected.abund)
+            projected.evenness <- hill.diversity(projected.abund)
+            
+            pos <- which(pert.sad$year.predicted == (as.numeric(initial.years[i.year]) + 1) &
+                           pert.sad$plot == plots[i.plot] & 
+                           pert.sad$type == types[i.type] &
+                           pert.sad$intensity == i.step)
+            
+            pert.sad$value[pos[which(pert.sad$metric[pos] == "composition")]] <- 
+              projected.composition
+            pert.sad$value[pos[which(pert.sad$metric[pos] == "abundance")]] <- 
+              projected.abundance
+            pert.sad$value[pos[which(pert.sad$metric[pos] == "evenness")]] <- 
+              projected.evenness
+          }# if !na
         }# for i.step
         
       }# if-else type
@@ -257,5 +284,9 @@ for(i.year in 1:length(initial.years)){
 }# for i.year
 
 
+# store results -----------------------------------------------------------
+
+write.csv2(pert.sad,file = "results/predicted_SAD_components.csv",
+           row.names = FALSE)
 
 
